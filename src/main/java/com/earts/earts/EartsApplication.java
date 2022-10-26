d292c2cdb35665e7d2655b31dec5107236c48558
Vinncent Wong <vinncentwong2@gmail.com>
2022-10-26 14:55:42 +0700

    init

---
+++ src/main/java/com/earts/earts/EartsApplication.java
@@ -0,0 +1,13 @@
+package com.earts.earts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/earts/earts/EartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earts.earts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
