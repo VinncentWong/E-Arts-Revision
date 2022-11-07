9bf44ce8c0ff7bec7217f9d4b90eda891af9331c
VinncentWong <vinncentwong2@gmail.com>
2022-11-07 19:22:01 +0700

    add artwork photo repo

---
+++ src/main/java/com/earts/earts/app/artwork/artwork_photo/ArtworkPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.earts.earts.app.artwork.artwork_photo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.earts.earts.entity.artwork.ArtworkPhoto;
+
+public interface ArtworkPhotoRepository extends CrudRepository<ArtworkPhoto, Long>{}
