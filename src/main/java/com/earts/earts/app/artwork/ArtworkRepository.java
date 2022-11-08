cdd1cf92ad3ef82c26d8d793fec9d6372585edea
VinncentWong <vinncentwong2@gmail.com>
2022-11-08 10:35:55 +0700

    add method to find artwork by artist id

--- src/main/java/com/earts/earts/app/artwork/ArtworkRepository.java
+++ src/main/java/com/earts/earts/app/artwork/ArtworkRepository.java
@@ -1,7 +1,14 @@
 package com.earts.earts.app.artwork;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.earts.earts.entity.artwork.Artwork;
 
-public interface ArtworkRepository extends CrudRepository<Artwork, String>{}
+public interface ArtworkRepository extends CrudRepository<Artwork, String>{
+
+    @Query(nativeQuery = true, value = "SELECT * FROM artwork WHERE artist_id = ?1")
+    Optional<Artwork> getArtworkByArtistId(Long artistId);
+}
