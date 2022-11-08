7b0103dc51e7a740bce49afc1d2e075ee22a2cc7
VinncentWong <vinncentwong2@gmail.com>
2022-11-08 09:53:12 +0700

    add artwork not founx exception

---
+++ src/main/java/com/earts/earts/exception/ArtworkNotFoundException.java
@@ -0,0 +1,12 @@
+package com.earts.earts.exception;
+
+public class ArtworkNotFoundException extends Exception{
+
+    public ArtworkNotFoundException() {
+    }
+
+    public ArtworkNotFoundException(String message) {
+        super(message);
+    }
+    
+}
